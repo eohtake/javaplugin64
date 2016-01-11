17d50247f6fdb23717065daf7494673a8161973e
Eric Sarsa <ericohtake@outlook.com>
2016-01-11 23:11:11 +0900

    Second release with some identation fixed

---
+++ pkg/ericohtake-javaplugin64-0.2.0/manifests/uninstall.pp
@@ -0,0 +1,19 @@
+class javaplugin64::uninstall (
+  $uninstall_string  = $javaplugin64::params::uninstall_string,
+  $uninstall_message = $javaplugin64::params::uninstall_message,
+) inherits javaplugin64::params {
+
+  exec { 'Uninstall Previous Version' :
+    command => "MsiExec.exe /qn /X${uninstall_string} /norestart",
+    path    => 'C:\windows\system32',
+  }
+
+  notify { 'Success! ':
+    name => $uninstall_message,
+  }
+
+  case $::javaplugin64 {
+    /\.*Java 8 Update 66/: { include javaplugin64::current_version_message }
+    default: {}
+  }
+}
